afc370eaac7dd83702e9603f737d341c03124287
ticso <ticso@6de3e79d-44c3-0310-81b2-c8aaf2c3f825>
2005-05-07 22:59:00 +0000

    first layout for serial driver
    git-svn-id: https://devel.bwct.de/svn/projects/modbus/usb/mb_tcpbridge-rs232@2085 6de3e79d-44c3-0310-81b2-c8aaf2c3f825

---
+++ mb_tcpbridge.cc
@@ -0,0 +1,365 @@
+/*
+ * Copyright (c) 2001 - 2004 Bernd Walter Computer Technology
+ * http://www.bwct.de
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $URL$
+ * $Date$
+ * $Author$
+ * $Rev$
+ */
+
+#include <bwct/bwct.h>
+
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/uio.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <termios.h>
+#include <unistd.h>
+
+#ifndef MAXSOCK
+#define MAXSOCK 10
+#endif
+
+int main(int argc, char *argv[]);
+void usage(void);
+
+static Mutex device_mtx;
+static int device;
+static int EP_in, EP_out;
+
+ssize_t
+writen(int fd, const void *vptr, size_t n) {
+	char *ptr = (char*)vptr;
+	fd_set fds;
+	size_t nleft = n;
+	ssize_t nwritten;
+	while (nleft > 0) {
+		FD_ZERO(&fds);
+		FD_SET(fd, &fds);
+		select(fd + 1, NULL, &fds, NULL, NULL);
+		if ((nwritten = write(fd, ptr, nleft)) < 0) {
+			if (errno != EAGAIN && errno != EINTR)
+				return(nwritten);
+			nwritten = 0;
+		}
+		nleft -= nwritten;
+		ptr += nwritten;
+	}
+	return (n);
+}
+
+class FConnect : public FTask {
+private:
+	union {
+		uint8_t data[258];
+		struct {
+			uint16_t refno;
+			uint8_t address;
+			uint8_t function;
+			uint8_t cmd[1];
+		};
+	} packet;
+	uint8_t packetlen;
+	uint8_t header[6];
+
+	void sendpacket();
+	void getpacket();
+	virtual void *threadstart();
+	virtual void threadend();
+	void work();
+
+	class Network : public ::Network::Net {
+	public:
+		Network(int nfd)
+		    : ::Network::Net(nfd) {}
+	};
+	uint16_t calccrc();
+	int checkcrc();
+
+public:
+        class Listen : public ::Network::Listen {
+	private:
+		virtual FTask *newtask();
+		virtual ::Network::Net * newcon(int clientfd);
+	};
+	FConnect() {
+	}
+	~FConnect() {
+	}
+
+};
+
+FTask *
+FConnect::Listen::newtask() {
+	return new FConnect();
+}
+
+::Network::Net *
+FConnect::Listen::newcon(int clientfd) {
+	cassert(clientfd >= 0);
+	socklen_t addrlen;
+	Matrix<char> addrdt(MAXSOCKADDR);
+	struct sockaddr *addr = (struct sockaddr*)addrdt.get();
+	addrlen = MAXSOCKADDR;
+	if (::getsockname(clientfd, addr, &addrlen) < 0)
+		throw Error("getsockname failed");
+	FConnect::Network* nobj;
+	nobj = new FConnect::Network(clientfd);
+	return nobj;
+}
+
+void *
+FConnect::threadstart() {
+	log("new connect");
+	// TODO do a type checking cast
+	((::Network::Net*)file.get())->nodelay(1);
+	work();
+	return NULL;
+}
+
+void
+FConnect::threadend() {
+	delete this;
+}
+
+uint16_t
+FConnect::calccrc() {
+	uint8_t pos;
+	uint8_t i;
+	int lsb;
+	uint16_t crc = 0xffff;
+	uint8_t len = packetlen - 2;
+
+	for (pos = 0; pos < len; pos++) {
+		crc ^= packet[pos];
+		for (i = 8; i ; i--) {
+			lsb = crc & 0x01;
+			crc = crc >> 1;
+			if (lsb)
+				crc ^= 0xa001;
+		}
+	}
+	return crc;
+}
+
+int
+FConnect::checkcrc() {
+	uint16_t crc = calccrc();
+	return ((packet[packetlen - 2] == (crc & 0xff)) &&
+	    (packet[packetlen - 1] == (crc >> 8)));
+}
+
+/*
+void
+FConnect::sendpacket() {
+
+	// TODO: check for errors
+//XXX	usb_bulk_write(device, EP_out, (char*)&packet.data[0], packetlen + 4, 1000);
+	return;
+}
+
+void
+FConnect::getpacket() {
+	int tmp;
+
+//XXX	tmp = usb_bulk_read(device, EP_in, (char*)&packet.data[0], 256, 1000);
+	if (tmp >= 0) 
+		packetlen = (uint8_t)(tmp - 4);
+}
+*/
+
+void
+FConnect::sendpacket() {
+	uint16_t crc;
+
+	packetlen += 2;
+	crc = calccrc();
+	packet[packetlen - 2] = crc & 0xff;
+	packet[packetlen - 1] = crc >> 8;
+	::writen(device, packet, packetlen);
+	tcflush(device, TCIFLUSH); // flush received data
+	return;
+}
+
+void
+FConnect::getpacket() {
+	int res;
+	fd_set fds;
+	struct timeval to;
+
+	packetlen = 0;
+	// wait for the first packet befor starting
+	FD_ZERO(&fds);
+	FD_SET(device, &fds);
+	to.tv_sec = 1;
+	to.tv_usec = 0; 
+	select(device + 1, &fds, NULL, NULL, &to);
+	if (!FD_ISSET(device, &fds)) {
+		printf("no slave response\n");
+		// XXX: TODO setuop correct exception
+		return;	// reaction timeout
+	}
+	// no range check needed because of overflow
+	for (packetlen = 0; ; ) {
+		FD_ZERO(&fds);
+		FD_SET(device, &fds);
+		to.tv_sec = 0;
+		to.tv_usec = 1000000/(115200/11*1.5);
+		res = select(device + 1, &fds, NULL, NULL, &to);
+		if (FD_ISSET(device, &fds))
+			packetlen += ::read(device, &packet[packetlen],
+			    256 - packetlen);	// TODO check for parity error
+		else {
+			// wait t3.5 to block other possible writes
+			usleep(1000000/(115200/11*3.5));
+			if (!checkcrc()) {
+				printf("received packet has wrong CRC\n");
+				// XXX: TODO setuop correct exception
+				packetlen = 0;
+			}
+			return;
+		}
+	}
+}
+
+void
+FConnect::work() {
+	ssize_t res;
+
+	for (;;) {
+		// TODO: timeout handling
+		res = file->read(&header, sizeof(header));
+		if (res < (ssize_t)sizeof(header)) {
+			return;
+		}
+		// TODO check header arguments;
+		packetlen = header[4] << 8 | header[5];
+		// TODO check packetlen;
+		res = file->read(&packet.data[2], packetlen);
+		if (res < packetlen) {
+			return;
+		}
+		packetlen -= 2;	// drop address and function bytes
+		// TODO add refno handling to ubmb
+		device_mtx.lock();
+		sendpacket();
+		getpacket();
+		device_mtx.unlock();
+		packetlen += 2; // add address and function bytes
+		header[0] = 0;
+		header[4] = 0;
+		header[5] = packetlen;
+		file->write(&header, sizeof(header));
+		file->write(&packet.data[2], packetlen);
+	}
+}
+
+void
+opensio(char* devname) {
+	struct termios buf;
+	int val;
+
+	device = open(devname, O_RDWR);
+	if (device < 0) {
+		printf("open serial %s failed: %s\n",
+		    devname, strerror(errno));
+		exit(1);
+	}
+	val = fcntl(device, F_GETFL, 0);
+	fcntl(device, F_SETFL, val | O_NONBLOCK);
+	if (tcgetattr(device, &buf) < 0) {
+		printf("tcgetattr failed: %s\n", strerror(errno));
+		exit (1);
+	}
+	cfmakeraw(&buf);
+	buf.c_iflag |= IGNBRK;
+	buf.c_cflag &= ~(CSIZE | PARODD);
+	buf.c_cflag |= CS8 | CLOCAL | PARENB /*| CSTOPB*/;
+	cfsetispeed(&buf, 115200);
+	cfsetospeed(&buf, 115200);
+	if (tcsetattr(device, TCSAFLUSH, &buf) < 0) {
+		printf("tcsetattr failed: %s\n", strerror(errno));
+		exit (1);
+	}
+}
+
+int
+main(int argc, char *argv[]) {
+	FConnect::Listen listen;
+	const char* serial;
+	int ch;
+
+	serial = NULL;
+
+	while ((ch = getopt(argc, argv, "i:ps:")) != -1)
+		switch (ch) {
+		case 's':
+			serial = optarg;
+			break;
+		case '?':
+		default:
+			usage();
+			/* NOTREACHED */
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 2 || serial == NULL)
+		usage();
+
+//XXX open serial
+	if (device == -1) {
+		printf("failed to open device\n");
+		exit(1);
+	}
+
+	listen.add_tcp(argv[0], argv[1]);
+	daemon(0,0);
+
+	listen.loop();
+	return 0;
+}
+
+void
+usage(void) {
+
+	printf("usage: tcpbridge -s serial ip port\n");
+	exit(1);
+}
+
